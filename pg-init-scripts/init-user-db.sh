1bf4023891e2409711b89d487213f6a0646a2c7c
xing.wenfeng <xing.wenfeng@uqam.ca>
2021-06-06 13:51:27 -0400

    all

--- pg-init-scripts/init-user-db.sh
+++ pg-init-scripts/init-user-db.sh
@@ -2,7 +2,14 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER docker WITH PASSWORD '12345';
-    CREATE DATABASE docker ;
-    GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+    create schema myschema;
+
+    CREATE TABLE myschema.accounts (
+        user_id serial PRIMARY KEY,
+        username VARCHAR ( 50 ) UNIQUE NOT NULL,
+        password VARCHAR ( 50 ) NOT NULL,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+            last_login TIMESTAMP
+    );
 EOSQL

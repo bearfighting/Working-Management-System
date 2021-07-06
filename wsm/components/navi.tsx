189f6b6a8a0818a2ef33eb5d8d9763c92da886c7
xing.wenfeng <xing.wenfeng@uqam.ca>
2021-07-05 21:20:19 -0400

    deconnecter

--- wsm/components/navi.tsx
+++ wsm/components/navi.tsx
@@ -1,10 +1,19 @@
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../static/logo.png';
 
 import Login from './login';
 
-export default function Navi() {
+export default function Navi({ connecte }) {
+  const handleDeconnecter = async () => {
+    await fetch("/api/authentification/deconnecter", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => {
+      window.location.href = 'http://localhost:3000/';
+    });
+  }
+
   return (
     <Navbar bg="light" expand="lg">
       {/* <Navbar.Brand href="/"><img style={{ height: 50, width: 50 }} src={logo} alt="Logo" /></Navbar.Brand> */}
@@ -16,8 +25,13 @@ export default function Navi() {
           <Nav.Link href="/pages/tache">Tâche</Nav.Link>
           <Nav.Link href="/pages/budget">Budget</Nav.Link>
         </Nav>
-        <Nav.Link style={{ marginRight: 10 }} href="/pages/inscription" className="btn btn-primary">Inscrire</Nav.Link>
-        <Nav.Link style={{ marginRight: 10 }} href="/pages/login" className="btn btn-primary">Connecter</Nav.Link>
+        {connecte ?
+          <Button onClick={handleDeconnecter}>Déconnecter</Button>
+          :
+          <>
+            <Nav.Link style={{ marginRight: 10 }} href="/pages/inscription" className="btn btn-primary">Inscrire</Nav.Link>
+            <Nav.Link style={{ marginRight: 10 }} href="/pages/login" className="btn btn-primary">Connecter</Nav.Link>
+          </>}
       </Navbar.Collapse>
     </Navbar>
   )

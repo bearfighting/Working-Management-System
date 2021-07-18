b5c8d3955a488670398bd53c83e267b36861cb02
xing.wenfeng <xing.wenfeng@uqam.ca>
2021-07-18 09:16:22 -0400

    ajouter le mailer

--- wsm/api/authentification/mailer.js
+++ wsm/api/authentification/mailer.js
@@ -1,3 +1,35 @@
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+
 module.exports = (req, res) => {
-    res.send("ok");
+    const { email } = req.body;
+
+    const resultat = await user_repo.findUser({ email });
+
+    if (resultat) {
+        const transporter = nodemailer.createTransport(smtpTransport({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
+            auth: {
+                user: 'miaxingyuanyuan@gmail.com',
+                pass: 'florentsophie520mia'
+            }
+        }));
+
+        const mailOptions = {
+            from: 'miaxingyuanyuan@gmail.com', // sender address
+            to: 'florentxingwenfeng@gmail.com', // list of receivers
+            subject: 'test mail', // Subject line
+            html: '<h1>this is a test mail.</h1>'// plain text body
+        };
+
+        transporter.sendMail(mailOptions, function (err, info) {
+            if (err)
+                res.sendStatus(404);
+            else
+                res.sendStatus(200);
+        })
+    } else {
+        res.sendStatus(403);
+    }
 }
